2efdbc91c910d285fb235c7b2ce8e3df4e3b5114
aplicaciones <aplicaciones@inverpacifico.com>
2023-03-08 21:33:34 -0500

    Ajuste de la consulta por mac en BNET

--- models/bdAlerta.js
+++ models/bdAlerta.js
@@ -1,17 +1,28 @@
 const { runQuery } = require("../database/config");
 
+const getPuntoVentaByMAC = async (mac = "") => {
+  //mac = "2c-f0-5d-eb-5-a9";
 
-const getPuntoVentaByMAC = async ( mac = '') => {
-
-   //mac = "2c-f0-5d-eb-5-a9";
-
-try {
+  try {
+    let dirMac = "";
+    //Si existen una coma en el string de la mac, quiere decir que vienen varias mac anidadas
+    if (mac.includes(",")) {
+      //Si existe una coma al final del string
+      if (mac.charAt(mac.length - 1) === ",") {
+        //Eliminar la ultima coma del string
+        dirMac = mac.substring(0, mac.length - 1);
+      } else {
+        dirMac = mac;
+      }
+    } else {
+      dirMac = "'" + mac + "'";
+    }
     sql = `
       SELECT PUNTOVENTA,nombre
       FROM GAMBLE.mac_punto_venta@apuestas, GAMBLE.territorios@apuestas 
-      WHERE lower(DIRECCION_MAC_EQUIPO) = '${mac}' AND PUNTOVENTA= CODIGO
+      WHERE lower(DIRECCION_MAC_EQUIPO) in (${dirMac}) AND PUNTOVENTA = CODIGO
        `;
-
+    //console.log(sql);
     result = await runQuery(sql);
 
     console.log(
@@ -25,7 +36,7 @@ try {
     if (result.resultQuery.rows.length > 0) {
       console.log("Listando los  Registros ");
       result.resultQuery.rows.map((data) => {
-        console.log(data[0],data[1]);
+        console.log(data[0], data[1]);
         codigoPDv = data[0];
         nombrePDV = data[1];
       });
@@ -34,35 +45,30 @@ try {
     }
 
     return {
-        nombre: nombrePDV,
-        codigo: codigoPDv
-    }
-    
+      nombre: nombrePDV,
+      codigo: codigoPDv,
+    };
   } catch (error) {
-    console.log('Se presenta error al consultar la mac en BNET' + error);
+    console.log("Se presenta error al consultar la mac en BNET" + error);
   }
-}
+};
 
-
-const insertAlerta = async ( mac, codigoPDV, nombrePDV, ip=null)  => {
-
-try {
-   
+const insertAlerta = async (mac, codigoPDV, nombrePDV, ip = null) => {
+  try {
     /***Registrar la alerta en la base de datos */
     sql = `
     INSERT INTO APPBOTONPANICO.ALERTA (FECHA, HORA,MAC,CODIGO_PDV,NOMBRE_PDV,IP) VALUES (SYSDATE,to_char(SYSDATE, 'HH24:MI:SS'),'${mac}','${codigoPDV}','${nombrePDV}','${ip}')
      `;
-    result = await runQuery(sql,[],true);
-
-    
+    result = await runQuery(sql, [], true);
   } catch (error) {
-    console.log('Se presenta error al Insertar el registro de la alerta en la base de datos' + error);
+    console.log(
+      "Se presenta error al Insertar el registro de la alerta en la base de datos" +
+        error
+    );
   }
-}
-
- 
+};
 
 module.exports = {
   getPuntoVentaByMAC,
-  insertAlerta
-}
+  insertAlerta,
+};
